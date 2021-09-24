8f191dc0461e5616a8f3bcfa74cefe3a6fe06d9e
Waleed <waleedamjad57@gmail.com>
2021-09-24 23:54:54 +0500

    Add secure password and attributes to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,7 @@ class User < ApplicationRecord
               presence: true, 
               length: { maximum: 50 },
               uniqueness: { case_sensitive: false }, 
-              format: { with: VALID_EMAIL_REGEX } 
+              format: { with: VALID_EMAIL_REGEX }
+
+has_secure_password
 end
